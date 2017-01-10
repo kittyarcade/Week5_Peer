71e70c376612ed4a53d6f4792dbde7b8a1ac48cf
Amanda Jopp <aclaire488@gmail.com>
2017-01-10 14:51:50 -0600

    angular route config steup

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,28 @@ var myApp = angular.module('myApp', []);
 myApp.controller('mainController', ['$scope', function($scope){
   console.log('totally angular dude');
 
+  myApp.config(['$routeProvider', function($routeProvider){
+    $routerProvider
+      .when('/home',{
+        templateUrl:'views/characters/home.html',
+        controller: 'homeController'
+      })
+      .when('/shaggy',{
+        templateUrl:'views/characters/shaggy.html',
+        controller: 'shaggyController'
+      })
+      .when('/bugs',{
+        templateUrl:'views/characters/bugs.html',
+        controller: 'bugsController'
+      })
+      .when('/wonder',{
+        templateUrl:'views/characters/wonder.html',
+        controller: 'wonderController'
+      })
+      .otherwise({
+        redirectTo: 'home'
+      });
+  }]); //end of config
 
 
 
