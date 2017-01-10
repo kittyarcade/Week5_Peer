fd906fb11975542603fa89d7df7160b7220ce153
Amanda Jopp <aclaire488@gmail.com>
2017-01-10 14:40:06 -0600

    initial html setup complete

---
+++ server/public/scripts/client.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', ['$scope', function($scope){
+  console.log('totally angular dude');
+
+
+
+
+}]); //end of mainController
